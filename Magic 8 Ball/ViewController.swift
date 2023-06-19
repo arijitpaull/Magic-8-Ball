c454c14cf7192eef1a9370fd41750fc5421a0e65
Arijit <arijitpaul141@gmail.com>
2023-06-19 23:06:32 +0530

    Implemented the basic functionality of the Magic 8 Ball game

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,18 +1,28 @@
-//
-//  ViewController.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ballImageView: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        self.becomeFirstResponder()
+    }
 
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            ballImageView.image = ballArray.randomElement()
+        }
+    }
+    
+    
 }
 
